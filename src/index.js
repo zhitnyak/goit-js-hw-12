b70fb2099a8a2a2519fb2b63879b135319c4ea79
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-07-13 22:04:49 +0300

    add_fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
+
+const nameCountry = fetch('https://restcountries.eu/rest/v2/name/germany')
+  .then(response => {
+    return response.json();
+  })
+  .then(name => {
+    console.log(name);
+  })
+  .catch(error => {
+    console.log(error);
+  });
